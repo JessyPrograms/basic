df787bcead863dc01daf78cc4ec2a20d190666af
JessyPrograms <ohthatsambartley@gmail.com>
2019-06-01 19:13:07 +0100

    update

---
+++ server/run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import server
+import importlib
+
+HOST = '127.0.0.1' # localhost
+PORT = 3000 # port to listen on
+
+while True:
+    importlib.reload(server)
+    server.Server(HOST, PORT)
--- server/server.py
+++ server/server.py
@@ -1,39 +1,34 @@
 #!/usr/bin/env python3
 
-import socket
 import importlib
+import socket
+
 import ServerParser
 
 
-HOST = '127.0.0.1' # localhost
-PORT = 3000 # port to listen on
-SERVER = (HOST, PORT)
+def Server(HOST, PORT):
+    clientList = []
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print("Listening on {}:{}".format(HOST, PORT))
+        conn, addr = s.accept()
+        cid = conn.recv(1024)
+        clientList.append(cid)
+        with conn:
+            print("Connection at {}!".format(addr[0]))
+            
+            while True:
+                importlib.reload(ServerParser)
+                data = conn.recv(1024)
 
-IPv4 = socket.AF_INET
-TCP = socket.SOCK_STREAM
+                if not data:
+                    break
 
-clientList = []
+                # sdata = response code
+                sdata = ServerParser.parse(data)
 
-with socket.socket(IPv4, TCP) as s:
-    s.bind(SERVER)
-    s.listen()
-    print("Listening on {}:{}".format(HOST, PORT))
-    conn, addr = s.accept()
-    cid = conn.recv(1024)
-    clientList.append((cid, addr))
-    with conn:
-        print("Connection at {}!".format(addr[0]))
-        
-        while True:
-            importlib.reload(ServerParser)
-            data = conn.recv(1024)
-
-            if not data:
-                break
-
-            sdata = ServerParser.parse(data)
-
-            if sdata != None:
-                conn.sendall(sdata)
-
-print("Connection Disconnected!")
+                if sdata != None:
+                    conn.sendall(sdata)
+    print("Connection Disconnected!")
+    return
