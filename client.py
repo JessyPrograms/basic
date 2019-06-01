2728fb1c9f7bcb81573316ef692098ef21c44be2
JessyPrograms <ohthatsambartley@gmail.com>
2019-06-01 13:30:36 +0100

    created client.py and edited server.py

---
+++ client.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import socket
+
+HOST, PORT = "127.0.0.1", 3000
+
+IPv4 = socket.AF_INET
+TCP = socket.SOCK_STREAM
+
+sdata = ""
+
+with socket.socket(IPv4, TCP) as s:
+    s.connect((HOST, PORT))
+    while sdata != "exit":
+        sdata = input("==> ").encode()
+        s.sendall(sdata)
+        data = s.recv(1024)
+        print(data)
---
+++ server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import socket
+
+HOST = '127.0.0.1' # localhost
+PORT = 3000 # port to listen on
+
+IPv4 = socket.AF_INET
+TCP = socket.SOCK_STREAM
+
+with socket.socket(IPv4, TCP) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print("Listening on {}:{}".format(HOST, PORT))
+    conn, addr = s.accept()
+    with conn:
+        print("Connection at {}!".format(addr[0]))
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print("Data received. size = {}".format(len(data)))
+            conn.sendall(data)
+    print("Connection Disconnected!")
